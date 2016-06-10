df5ebef70ac3962ebb8ec9ad6cc864586fcd923d
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2016-06-10 11:47:31 -0400

    more url

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,13 +1,13 @@
 module.exports = {
   staticFileGlobs: [
-    '/index.html',
-    '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
-    '/data/evolutions.js',
-    '/data/details.js',
-    '/data/cooldowns.js',
-    '/data/units.js',
-    '/data/aliases.js'
+    'index.html',
+    'manifest.json',
+    'bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    'data/evolutions.js',
+    'data/details.js',
+    'data/cooldowns.js',
+    'data/units.js',
+    'data/aliases.js'
   ],
   navigateFallback: '/index.html'
 };
